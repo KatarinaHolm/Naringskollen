f8f2b1458eec00d174a40c7425a168e09b6ddb04
Katarina Holm <katarinaholm89@gmail.com>
2026-08-29 13:22:29 +0200

    Add project files.

---
+++ Data/NaringskollenDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Naringskollen.Data
+{
+    public class NaringskollenDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+        public NaringskollenDbContext(DbContextOptions<NaringskollenDbContext> options) : base(options)
+        {
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Naringskollen.Data;
+using Scalar.AspNetCore;
+
+namespace Naringskollen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<NaringskollenDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            builder.Services.AddIdentityApiEndpoints<IdentityUser<int>>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddRoles<IdentityRole<int>>()
+                .AddEntityFrameworkStores<NaringskollenDbContext>();
+
+            builder.Services.AddControllers();
+
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.MapScalarApiReference();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
