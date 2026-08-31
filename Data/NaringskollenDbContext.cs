e6b54142af2a946b98cfd2a2d8f7ef5a36a16fe6
Katarina Holm <katarinaholm89@gmail.com>
2026-08-31 20:26:44 +0200

    Feat: Configure DbContext, add IdentityRole Admin.

--- Data/NaringskollenDbContext.cs
+++ Data/NaringskollenDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Naringskollen.Models;
 
 namespace Naringskollen.Data
 {
@@ -8,7 +9,26 @@ namespace Naringskollen.Data
     {
         public NaringskollenDbContext(DbContextOptions<NaringskollenDbContext> options) : base(options)
         {
-           
+
+        }
+
+        public DbSet<FoodMeasurement> FoodMeasurements { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Food> Foods { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole<int>>().HasData(
+                new IdentityRole<int>
+                {
+                    Id = 1,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                });
         }
     }
 }
