d052cd0a57bfb8adf76cbf0d2b75bfdb931861a9
Katarina Holm <katarinaholm89@gmail.com>
2026-09-22 14:59:06 +0200

    feat: auth controller with logout endpoint. Was first thinking to do Minimal Api but changed to an own controller.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Naringskollen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser<int>> signInManager;
+
+        public AuthController(SignInManager<IdentityUser<int>> _signInManager)
+        {
+            signInManager = _signInManager;
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout([FromBody] object empty)
+        {
+            if (empty != null)
+            {
+                await signInManager.SignOutAsync();
+                return Ok();
+            }
+            return Unauthorized();
+        }
+    }
+}
